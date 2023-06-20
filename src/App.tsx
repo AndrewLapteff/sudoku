e13d0a6a5e6f84dd1ef8056a3321a58acb1ec91e
Andrew Lapteff <this.is.amigogo@gmail.com>
2023-06-20 20:38:34 +0300

    Added difficulty setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
-import Sudoku, { solveSudoku } from './Sudoku'
+import { useEffect, useRef, useState } from 'react'
+import Sudoku, { solveSudoku } from './sudoku'
 import { TMatrix } from './types/Matrix'
 import './App.css'
 
@@ -7,18 +7,24 @@ const initialArray = new Array(9).fill(null).map(() => new Array(9).fill(0))
 
 function App() {
   const [numbers, setNumbers] = useState<TMatrix>(initialArray)
+  const [isStarted, setStarted] = useState(false)
   const [errorsCount, setErrorsCount] = useState(0)
   const [result, setResult] = useState<TMatrix>(initialArray)
+  const [emptyCells, setEmptyCells] = useState(0)
+  const inputRef = useRef<HTMLInputElement>(null)
 
-  useEffect(() => {
-    const sudoku = new Sudoku(9, 40)
+  const startup = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
+    if (emptyCells < 1 || emptyCells > 81) return
+    const sudoku = new Sudoku(9, emptyCells)
     sudoku.fillValues() // створення початкових значеннь
     const sudokuMap = sudoku.mat // початкова матриця
     const result = structuredClone(sudokuMap)
     solveSudoku(result, 0, 0) // метод котрмй мутує массив та вирішує судоку
     setResult(result)
     setNumbers(sudokuMap)
-  }, [])
+    setStarted(true)
+  }
 
   const decorationCheck = (colIdx: number, rowIdx: number) => {
     colIdx++
@@ -39,15 +45,23 @@ function App() {
     )
   }
 
+  const savingProgressHandler = (newGrid: TMatrix) => {
+    let progress = ''
+    newGrid.forEach((numbersArray) => {
+      progress += ',' + numbersArray.join(',')
+    })
+    progress = progress.slice(1, progress.length)
+    localStorage.setItem('progress', progress)
+  }
+
   const fillCell = (rowIdx: number, colIdx: number, value: number): void => {
-    if (value > 9) {
-      value = value / 10
-    }
+    if (value > 9) value = value / 10
     // перевірка на корректність відповіді
     if (value == result[rowIdx][colIdx]) {
       const newGrid = [...numbers]
       newGrid[rowIdx][colIdx] = value
       setNumbers(newGrid)
+      savingProgressHandler(newGrid)
     } else {
       setErrorsCount((prev) => prev + 1)
     }
@@ -57,8 +71,33 @@ function App() {
     setNumbers(result)
   }
 
-  // перевірка на заповнення матриці
-  if (numbers[0].length != 0)
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [])
+
+  if (!isStarted) {
+    return (
+      <>
+        <h3>Кількість пустих клітинок</h3>
+        <form>
+          <input
+            className="cells-count-input"
+            value={emptyCells}
+            ref={inputRef}
+            onChange={(e) => setEmptyCells(parseInt(e.target.value) || 0)}
+            type="text"
+          />
+          <button
+            type="submit"
+            className="solve-btn"
+            onClick={(e) => startup(e)}
+          >
+            Грати
+          </button>
+        </form>
+      </>
+    )
+  } else {
     return (
       <>
         <h2>{errorsCount}</h2>
@@ -97,6 +136,7 @@ function App() {
         </button>
       </>
     )
+  }
 }
 
 export default App
